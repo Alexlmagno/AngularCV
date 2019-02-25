2b45e9f0111b503cef8850d26855e0880a099cb1
Alejandro Lopez <alopezamado@deloitte.es>
2019-02-25 12:45:31 +0100

    Make possible to have diferents carousels in the same page controlled by one controller

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,5 +10,10 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  changeCV() {
+    console.log('click');
+    $('.carousel').carousel('next');
+  }
 
 }
