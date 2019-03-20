75540edd63f441549a76704baa7ddf495ecb53bb
Alejandro Lopez <alopezamado@deloitte.es>
2019-03-20 12:01:30 +0100

    Added svg icon and animation. Solved animaiton bug

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TypeService } from '../type.service';
+import { _ } from 'underscore';
 import * as anime from 'animejs';
 declare var $: any;
 @Component({
@@ -9,25 +10,38 @@ declare var $: any;
 })
 export class NavbarComponent implements OnInit {
   type: String;
+  loops: number;
 
   constructor(private typeService: TypeService) { }
 
   ngOnInit() {
     this.getType();
+    this.loops = 1;
   }
 
   changeCV() {
     this.typeService.changeType();
     this.getType();
+
     $('.carousel').carousel('next');
-    var colorsExamples = anime.timeline({
+
+    anime.timeline({
       endDelay: 1000,
       easing: 'easeInOutQuad',
       direction: 'alternate',
       loop: false
     })
     .add({ targets: '.navbar',  background: this.type === 'Eng' ? 'rgb(158, 165, 206)' : 'rgb(206, 158, 158)'}, 0);
+
+    anime({
+      targets: '.icon',
+      rotate: `${this.loops++}turn`,
+      duration: 1000,
+      easing: 'easeInOutCirc'
+    });
   }
+
+  throttleChangeCV = _.throttle(this.changeCV,1000);
 
   getType(): void {
     this.type = this.typeService.getType();
